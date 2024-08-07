5179c789ebb25aef98fd3f9268c1a34762f28616
Fabco <contact@fabco.tech>
2024-08-07 18:13:42 +0200

    new dependencies for jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  collectCoverageFrom: ['src/**/*.ts'],
+};
